72a6947ba6f228f2e350bff22af21a33d049751b
Michele Nasti <michele.nasti@gmail.com>
2018-06-07 00:09:00 +0200

    shipping and hst

--- assets/js/order.js
+++ assets/js/order.js
@@ -21,16 +21,20 @@ Vue.component('prices-table', {
     },
     computedTotalPrice: function () {
       const computedTotalPrice = this.products.map(product => {
-        let price = 0; 
-        if (!product.quantity) return {price, hst: 0, weight: 0};
+        let price = 0;
+        if (!product.quantity) return {
+          price,
+          hst: 0,
+          weight: 0
+        };
         if (this.isRetail()) {
           price = product.quantity * product.price_retailers
         } else {
           price = product.quantity * product.price
         }
         return {
-          price, 
-          hst: product.hst ? price*this.hstTaxRate : 0,
+          price,
+          hst: product.hst ? price * this.hstTaxRate : 0,
           weight: product.quantity * KG_TO_LBS
         }
       });
@@ -109,7 +113,8 @@ var vm = new Vue({
     grandTotal: 0,
     hstTaxRate: 0,
     shippingRates: {},
-    shipping: null
+    selectedShipping: null,
+    fuelSurcharge: {}
   },
   computed: {
     totalQuantity: function () {
@@ -121,35 +126,96 @@ var vm = new Vue({
           if (!product || !product.quantity) return sum
           return sum + Number(product.quantity)
         }, 0)
-    }, 
-    isOrderAllowed: function() {
-      return (this.totalQuantity != 0 && this.totalQuantity % 20 === 0) 
     },
-    hstTax: function() {
+    isOrderAllowed: function () {
+      return (this.totalQuantity != 0 && this.totalQuantity % 20 === 0)
+    },
+    weightTotal: function () {
       return this.barnyardComputedTotalPrice
-      .concat(this.generalSeedComputedTotalPrice)
-      .concat(this.bioAgComputedTotalPrice)
-      .reduce(function (sum, prices) {
-        if (!prices || !prices.hst) return sum
-        return sum + (prices.hst/100)
-      }, 0);
+        .concat(this.generalSeedComputedTotalPrice)
+        .concat(this.bioAgComputedTotalPrice)
+        .reduce(function (sum, prices) {
+          if (!prices || !prices.weight) return sum
+          return sum + (prices.weight)
+        }, 0);
     },
-    weightTotal: function() {
-      return this.barnyardComputedTotalPrice
-      .concat(this.generalSeedComputedTotalPrice)
-      .concat(this.bioAgComputedTotalPrice)
-      .reduce(function (sum, prices) {
-        if (!prices || !prices.weight) return sum
-        return sum + (prices.weight)
-      }, 0);
-    },
-    shippingPrice: function() {
-      _.find(this.shippingRates, function(o) {
-        console.log(this.shipping)
-        return o.location === this.shipping 
+    shippingLocation: function () {
+      //first, let's find the shipping location from the select. 
+      let item = null;
+      const selectedShipping = this.selectedShipping;
+      Object.keys(this.shippingRates).forEach(region => {
+        const locations = this.shippingRates[region]
+        locations.forEach(shippingRate => {
+          if (shippingRate.location === selectedShipping) {
+            item = shippingRate
+          }
         })
-      return 0
-    }
+      })
+      if (!item) return;
+      return item
+    },
+    shippingPrice() {
+      //This function calculates the shipping price based on the location selected. 
+      if (!this.shippingLocation) return 0
+      //first, transform the object with prices in a simpler data structure. 
+      // move prom "price-1000: 3.55" to an object { key: 1000, value: 3.55 }
+      const arrayOfPrices = []
+      Object.keys(this.shippingLocation)
+        .filter(key => key.startsWith("price-"))
+        .forEach(price => {
+          arrayOfPrices.push({
+            key: Number(price.substring(6)),
+            value: Number(this.shippingLocation[price])
+          })
+        })
+
+      // sort this array of prices by key.
+      arrayOfPrices.sort((item1, item2) => {
+        return item1.key - item2.key
+      })
+      
+      // compare the total weight against the key. 
+      // If the weight is more than the key, update the priceToReturn.
+      // if the weight is greater than the key, we skip the check 
+      // (we have set the value at the preceeding check)
+      let priceFor100Lbs = 0;
+      arrayOfPrices.forEach(elem => {
+        if (this.weightTotal >= elem.key) priceFor100Lbs = elem.value
+        if (this.weightTotal < elem.key) return;
+      })
+
+      const fuelSurcharge = this.weightTotal < 10000 
+      ? this.fuelSurcharge.fuelSurcharge 
+      : this.fuelSurcharge.fuelSurcharge1000
+      
+      // Now we finally calculate the shipping for this weight
+      const priceToReturn = ((priceFor100Lbs * this.weightTotal)/100)*fuelSurcharge
+
+      console.log("----------------------------------")
+      console.log("total weight: ", this.weightTotal)
+      console.log("price per 100 lbs: ", priceFor100Lbs),
+      console.log("shippingPrice: ", priceToReturn),
+      console.log("fuelSurcharge: ", fuelSurcharge)
+      return priceToReturn;
+    },
+    shippingHST() {
+      const shippingHST = this.shippingPrice*this.hstTaxRate/100
+      return shippingHST
+    },
+    hstTax: function () {
+      const hstForProducts = this.barnyardComputedTotalPrice
+        .concat(this.generalSeedComputedTotalPrice)
+        .concat(this.bioAgComputedTotalPrice)
+        .reduce(function (sum, prices) {
+          if (!prices || !prices.hst) return sum
+          return sum + (prices.hst / 100)
+        }, 0);
+        console.log("HST for products: ", hstForProducts)
+        console.log("HST for Shipping: ", this.shippingHST)
+        const hstTotal = hstForProducts + this.shippingHST
+        console.log("HST total: ", hstTotal)
+        return hstTotal
+    },
   },
   watch: {
     //using watch because computed properties can't detect modifications in arrays.
@@ -161,34 +227,54 @@ var vm = new Vue({
     },
     bioAgComputedTotalPrice: function () {
       this.computeGrandTotal()
-    }
+    },
+    // selectedShipping: function() { 
+    //   _.find(this.shippingRates, function(o) {
+    //     return o.location === this.selectedShipping 
+    //     })
+    //     console.log(this.selectedShipping)
+    //   return 0
+    // }
   },
   methods: {
     isRetail: function () {
       return this.purchaser === 'retail' && this.companyName && this.companyName.trim().length > 0
     },
     computeGrandTotal: function () {
-      this.grandTotal =
-        this.barnyardComputedTotalPrice
+      const itemsTotal = this.barnyardComputedTotalPrice
         .concat(this.generalSeedComputedTotalPrice)
         .concat(this.bioAgComputedTotalPrice)
         .reduce(function (sum, prices) {
           if (!prices || !prices.price) return sum
           return sum + prices.price
         }, 0);
+        const grandTotal = itemsTotal + this.shippingPrice + this.hstTax
     }
   },
   created: function () {
-    this.barnyardOrganicsProducts = _orderForm.barnyard_organics.products
-    this.generalSeedProducts = _orderForm.general_seed.products
-    this.bioAgProducts = _orderForm.bio_ag.products
+
+    function convertPricesToNumbers(product) {
+      return {
+        ...product,
+        price: Number(product.price),
+        price_retailers: Number(product.price_retailers)
+      }
+    }
+
+    this.barnyardOrganicsProducts = _orderForm.barnyard_organics.products.map(convertPricesToNumbers);
+    this.generalSeedProducts = _orderForm.general_seed.products.map(convertPricesToNumbers);
+    this.bioAgProducts = _orderForm.bio_ag.products.map(convertPricesToNumbers);
     this.hstTaxRate = Number(_orderForm.hst)
     this.fuelSurcharge = {
       fuelSurcharge: Number(_shippingRates.fuel_surcharge),
-      fuelSurcharge1000: Number(_shippingRates.fuel_surcharge_10000) 
+      fuelSurcharge1000: Number(_shippingRates.fuel_surcharge_10000)
 
     }
-    const { fuel_surcharge, fuel_surcharge_10000, ...shippingRates } = _shippingRates
+    const {
+      fuel_surcharge,
+      fuel_surcharge_10000,
+      ...shippingRates
+    } = _shippingRates
     this.shippingRates = shippingRates
   }
 })

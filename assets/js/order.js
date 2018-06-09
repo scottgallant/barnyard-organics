1f3e1998671d0bcf43df178afc490879f81e50b7
Michele Nasti <michele.nasti@gmail.com>
2018-06-09 16:36:56 +0200

    $form log

--- assets/js/order.js
+++ assets/js/order.js
@@ -272,8 +272,9 @@ var vm = new Vue({
     submitForm() {
       $("#order-form").submit(function(e) {
         e.preventDefault();
-      
+        
         var $form = $(this);
+        console.log($form);
         $.post($form.attr("action"), $form.serialize()).then(function() {
           alert("Thank you!");
         });
